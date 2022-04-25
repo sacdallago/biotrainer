aff6cd286fc54cea9a9d9fa4bad4cb027f2a7d55
Sebastian Franz <sebastian11@online.de>
2022-04-25 12:29:27 +0200

    Adding SequenceEmbeddingsDataset
    Uses torch.mean to calculate mean of embeddings for one sequence

---
+++ biotrainer/datasets/SequenceEmbeddingsDataset.py
@@ -0,0 +1,23 @@
+import torch
+
+from typing import Tuple
+
+
+class SequenceEmbeddingsDataset(torch.utils.data.Dataset):
+    def __init__(self, samples):
+        self.ids = list()
+        self.inputs = list()
+        self.targets = list()
+        for seq_id, (inputs, targets) in samples.items():
+            self.ids.append(seq_id)
+            self.inputs.append(torch.mean(inputs, dim=0))
+            self.targets.append(targets)
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index: int) -> Tuple[str, torch.FloatTensor, torch.LongTensor]:
+        seq_id = self.ids[index]
+        x = self.inputs[index].float()
+        y = self.targets[index].long()
+        return seq_id, x, y
