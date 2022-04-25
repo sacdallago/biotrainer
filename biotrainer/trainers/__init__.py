e11b0be03d8a7254e3957381ee6cdb625e01c98f
Sebastian Franz <sebastian11@online.de>
2022-04-25 12:33:01 +0200

    Adding sequence to class file
    Mimics functionality in residue_to_class with adaptions for sequence based predictions (especially handling class labels)

--- biotrainer/trainers/__init__.py
+++ biotrainer/trainers/__init__.py
@@ -1,5 +1,7 @@
 from .residue_to_class import residue_to_class
+from .sequence_to_class import sequence_to_class
 
 __all__ = [
-    'residue_to_class'
+    'residue_to_class',
+    'sequence_to_class'
 ]
