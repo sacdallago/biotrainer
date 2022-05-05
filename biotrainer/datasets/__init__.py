c01a728738d962f8a996fe89be936975cfee7628
Sebastian Franz <sebastian.franz@tum.de>
2022-05-05 18:54:30 +0200

    Refactoring dataset module to use factory function get_dataset
    Like for the models module, use get_dataset function to get the correct dataset for the chosen protocol

--- biotrainer/datasets/__init__.py
+++ biotrainer/datasets/__init__.py
@@ -2,9 +2,22 @@ from .collate_functions import pad_sequences
 from .ResidueEmbeddingsDataset import ResidueEmbeddingsDataset
 from .SequenceEmbeddingsDataset import SequenceEmbeddingsDataset
 
+__DATASETS = {
+    'residue_to_class': ResidueEmbeddingsDataset,
+    'sequence_to_class': SequenceEmbeddingsDataset
+}
+
+
+def get_dataset(protocol: str, samples: dict):
+    dataset = __DATASETS.get(protocol)
+
+    if not dataset:
+        raise NotImplementedError
+    else:
+        return dataset(samples=samples)
+
 
 __all__ = [
     'pad_sequences',
-    'ResidueEmbeddingsDataset',
-    'SequenceEmbeddingsDataset',
+    'get_dataset',
 ]
