608323129cbc5114e4bbda2de4c7080f80dd1513
Sebastian Franz <sebastian.franz@tum.de>
2022-05-06 15:46:33 +0200

    Refactoring solver module to use factory function get_solver
    Getting consistency with other modules

--- biotrainer/solvers/__init__.py
+++ biotrainer/solvers/__init__.py
@@ -1,7 +1,22 @@
 from .ResidueSolver import ResidueSolver
 from .SequenceSolver import SequenceSolver
 
+
+__SOLVERS = {
+    'residue_to_class': ResidueSolver,
+    'sequence_to_class': SequenceSolver
+}
+
+
+def get_solver(protocol: str, **kwargs):
+    solver = __SOLVERS.get(protocol)
+
+    if not solver:
+        raise NotImplementedError
+    else:
+        return solver(**kwargs)
+
+
 __all__ = [
-    'ResidueSolver',
-    'SequenceSolver'
+    'get_solver'
 ]
