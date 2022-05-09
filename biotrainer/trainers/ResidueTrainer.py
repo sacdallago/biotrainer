21109c4870571e1f68fe998253467ab577bc49ad
Sebastian Franz <sebastian.franz@tum.de>
2022-05-09 17:22:06 +0200

    Refactoring trainer classes in order to be more object-oriented
    Using self. for all attributes (where it makes sense), thus removing all parameters from the methods. Also to have the main workflow in the execute_pipeline more clear.

--- biotrainer/trainers/ResidueTrainer.py
+++ biotrainer/trainers/ResidueTrainer.py
@@ -11,60 +11,43 @@ class ResidueTrainer(Trainer):
 
     @staticmethod
     def pipeline(**kwargs):
-        return ResidueTrainer()._execute_pipeline(**kwargs)
+        return ResidueTrainer(**kwargs)._execute_pipeline()
 
-    def _load_sequences_and_labels(self, sequence_file, labels_file):
+    def _load_sequences_and_labels(self):
         # Parse FASTA protein sequences
-        protein_sequences = read_FASTA(sequence_file)
-        id2fasta = {protein.id: str(protein.seq) for protein in protein_sequences}
+        protein_sequences = read_FASTA(self.sequence_file)
+        self.id2fasta = {protein.id: str(protein.seq) for protein in protein_sequences}
 
         # Parse label sequences
-        label_sequences = read_FASTA(labels_file)
-        id2label = {label.id: str(label.seq) for label in label_sequences}
+        label_sequences = read_FASTA(self.labels_file)
+        self.id2label = {label.id: str(label.seq) for label in label_sequences}
         # Get the sets of training, validation and testing samples
-        training_ids, validation_ids, testing_ids = get_sets_from_labels(label_sequences)
+        self.training_ids, self.validation_ids, self.testing_ids = get_sets_from_labels(label_sequences)
 
-        return training_ids, validation_ids, testing_ids, id2label, id2fasta
-
-    def _generate_class_labels(self, id2label, id2fasta):
+    def _generate_class_labels(self):
         # Infer classes from data
-        class_labels = set()
-        for classes in id2label.values():
-            class_labels = class_labels | set(classes)
+        self.class_labels = set()
+        for classes in self.id2label.values():
+            self.class_labels = self.class_labels | set(classes)
 
         # Create a mapping from integers to class labels and reverse
-        class_str2int = {letter: idx for idx, letter in enumerate(class_labels)}
-        class_int2str = {idx: letter for idx, letter in enumerate(class_labels)}
+        self.class_str2int = {letter: idx for idx, letter in enumerate(self.class_labels)}
+        self.class_int2str = {idx: letter for idx, letter in enumerate(self.class_labels)}
 
         # Convert label values to lists of numbers based on the maps
-        id2label = {identifier: np.array([class_str2int[label] for label in labels])
-                    for identifier, labels in id2label.items()}  # classes idxs (zero-based)
+        self.id2label = {identifier: np.array([self.class_str2int[label] for label in labels])
+                         for identifier, labels in self.id2label.items()}  # classes idxs (zero-based)
 
         # Make sure the length of the sequences in the FASTA matches the length of the sequences in the labels
-        for seq_id, seq in id2fasta.items():
-            if len(seq) != id2label[seq_id].size:
-                Exception(f"Length mismatch for {seq_id}: Seq={len(seq)} VS Labels={id2label[seq_id].size}")
+        for seq_id, seq in self.id2fasta.items():
+            if len(seq) != self.id2label[seq_id].size:
+                Exception(f"Length mismatch for {seq_id}: Seq={len(seq)} VS Labels={self.id2label[seq_id].size}")
 
-        return class_labels, id2label, class_int2str, class_str2int
+    def _use_reduced_embeddings(self) -> bool:
+        return False
 
-    def _get_embeddings_config_and_file_name(self, sequence_file, output_dir, embedder_name):
-        embeddings_config = {
-            "global": {
-                "sequences_file": sequence_file,
-                "prefix": str(output_dir / embedder_name),
-                "simple_remapping": True
-            },
-            "embeddings": {
-                "type": "embed",
-                "protocol": embedder_name
-            }
-        }
-        embeddings_file_name = "embeddings_file.h5"
-
-        return embeddings_config, embeddings_file_name
-
-    def _get_number_features(self, id2emb, training_ids):
-        return torch.tensor(id2emb[training_ids[0]]).shape[1]
+    def _get_number_features(self):
+        return torch.tensor(self.id2emb[self.training_ids[0]]).shape[1]
 
     def _get_collate_function(self):
         return pad_sequences
--- biotrainer/trainers/SequenceTrainer.py
+++ biotrainer/trainers/SequenceTrainer.py
@@ -10,49 +10,31 @@ class SequenceTrainer(Trainer):
 
     @staticmethod
     def pipeline(**kwargs):
-        return SequenceTrainer()._execute_pipeline(**kwargs)
+        return SequenceTrainer(**kwargs)._execute_pipeline()
 
-    def _load_sequences_and_labels(self, sequence_file, labels_file):
+    def _load_sequences_and_labels(self):
         # Parse FASTA protein sequences
-        protein_sequences = read_FASTA(sequence_file)
-        id2fasta = {protein.id: str(protein.seq) for protein in protein_sequences}
+        protein_sequences = read_FASTA(self.sequence_file)
+        self.id2fasta = {protein.id: str(protein.seq) for protein in protein_sequences}
         # Get the sets of labels, training, validation and testing samples
-        id2label, training_ids, validation_ids, testing_ids = get_sets_from_single_fasta(protein_sequences)
+        self.id2label, \
+            self.training_ids, self.validation_ids, self.testing_ids = get_sets_from_single_fasta(protein_sequences)
 
-        return training_ids, validation_ids, testing_ids, id2label, id2fasta
-
-    def _generate_class_labels(self, id2label, id2fasta):
+    def _generate_class_labels(self):
         # Infer classes from data
-        class_labels = set(id2label.values())
+        self.class_labels = set(self.id2label.values())
         # Create a mapping from integers to class labels and reverse
-        class_str2int = {letter: idx for idx, letter in enumerate(class_labels)}
-        class_int2str = {idx: letter for idx, letter in enumerate(class_labels)}
+        self.class_str2int = {letter: idx for idx, letter in enumerate(self.class_labels)}
+        self.class_int2str = {idx: letter for idx, letter in enumerate(self.class_labels)}
         # Convert label values to lists of numbers based on the maps
-        id2label = {identifier: np.array(class_str2int[label])
-                    for identifier, label in id2label.items()}  # classes idxs (zero-based)
+        self.id2label = {identifier: np.array(self.class_str2int[label])
+                         for identifier, label in self.id2label.items()}  # classes idxs (zero-based)
 
-        return class_labels, id2label, class_int2str, class_str2int
+    def _use_reduced_embeddings(self) -> bool:
+        return True
 
-    def _get_embeddings_config_and_file_name(self, sequence_file, output_dir, embedder_name):
-        embeddings_config = {
-            "global": {
-                "sequences_file": sequence_file,
-                "prefix": str(output_dir / embedder_name),
-                "simple_remapping": True
-            },
-            "embeddings": {
-                "type": "embed",
-                "protocol": embedder_name,
-                "reduce": True,
-                "discard_per_amino_acid_embeddings": True
-            }
-        }
-        embeddings_file_name = "reduced_embeddings_file.h5"
-
-        return embeddings_config, embeddings_file_name
-
-    def _get_number_features(self, id2emb, training_ids):
-        return torch.tensor(id2emb[training_ids[0]]).shape[0]
+    def _get_number_features(self):
+        return torch.tensor(self.id2emb[self.training_ids[0]]).shape[0]
 
     def _get_collate_function(self):
         return None
