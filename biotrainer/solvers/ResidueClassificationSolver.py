0064c6d601860427a14d4834b33cd6beaebfc9c6
Sebastian Franz <sebastian.franz@tum.de>
2022-05-23 15:01:23 +0200

    Fixing that prediction is correctly permuted

--- biotrainer/solvers/ResidueClassificationSolver.py
+++ biotrainer/solvers/ResidueClassificationSolver.py
@@ -7,7 +7,7 @@ class ResidueClassificationSolver(Solver):
     def _transform_prediction_output(self, prediction):
         network_type = type(self.network).__name__
         if network_type in ["FNN", "LogReg"] :
-            prediction.permute(0, 2, 1)  # (Batch_size x protein_Length x Number_classes) => (B x N x L)
+            prediction = prediction.permute(0, 2, 1)  # (Batch_size x protein_Length x Number_classes) => (B x N x L)
 
         prediction_probabilities = torch.softmax(prediction, dim=1)
         _, predicted_classes = torch.max(prediction_probabilities, dim=1)
