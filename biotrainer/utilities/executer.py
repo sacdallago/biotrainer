701e327bf979ad7fae260a6e9bcb34134dacce88
Sebastian Franz <sebastian.franz@tum.de>
2022-05-23 14:36:52 +0200

    Moving config related verification functions and error to config.py
    Making the structure more clear and cleaning up unnecessary functions

--- biotrainer/utilities/executer.py
+++ biotrainer/utilities/executer.py
@@ -5,68 +5,10 @@ from copy import deepcopy
 from typing import Dict, Any
 from pathlib import Path
 
-from ..models import __MODELS
+from .config import *
 from ..trainers import get_trainer
-from .config import read_config_file, write_config_file
 
 logger = logging.getLogger(__name__)
-
-
-class ConfigurationException(Exception):
-    pass
-
-
-def _validate_file(file_path: str):
-    """
-    Verify if a file exists and is not empty.
-    Parameters
-    ----------
-    file_path : str
-        Path to file to check
-    Returns
-    -------
-    bool
-        True if file exists and is non-zero size,
-        False otherwise.
-    """
-    try:
-        if os.stat(file_path).st_size == 0:
-            raise ConfigurationException(f"The file at '{file_path}' is empty")
-    except (OSError, TypeError) as e:
-        raise ConfigurationException(f"The configuration file at '{file_path}' does not exist") from e
-
-
-def _verify_config(config: dict):
-    protocol = config["protocol"]
-    model = config["model_choice"]
-
-    if protocol == 'residue_to_class':
-        try:
-            labels_file = config["labels_file"]
-            sequence_file = config["sequence_file"]
-        except KeyError as e:
-            raise ConfigurationException("Correct files not available for protocol: " + protocol)
-
-    elif protocol == 'sequence_to_class':
-        try:
-            sequence_file = config["sequence_file"]
-        except KeyError as e:
-            raise ConfigurationException("Correct files not available for protocol: " + protocol)
-
-        if "labels_file" in config.keys() and config["labels_file"] != "":
-            raise ConfigurationException("Labels are expected to be found in the sequence file for protocol: "
-                                         + protocol)
-
-    if model not in __MODELS.get(protocol):
-        raise ConfigurationException( "Model " + model + " not available for protocol: " + protocol)
-
-
-def _convert_paths_to_absolute(config: dict, input_file_path: Path):
-    if "labels_file" in config.keys():
-        config["labels_file"] = input_file_path / config["labels_file"]
-    if "sequence_file" in config.keys():
-        config["sequence_file"] = input_file_path / config["sequence_file"]
-    return config
 
 
 __PROTOCOLS = {
@@ -87,14 +29,17 @@ def execute(output_dir: Path = "output", protocol: str = "residue_to_class", **k
 
 
 def parse_config_file_and_execute_run(config_file_path: str):
-    _validate_file(config_file_path)
+    validate_file(config_file_path)
 
     # read configuration and execute
     config = read_config_file(config_file_path)
-    _verify_config(config)
+    verify_config(config)
 
     input_file_path = Path(os.path.dirname(os.path.abspath(config_file_path)))
-    config = _convert_paths_to_absolute(config, input_file_path)
+    if "labels_file" in config.keys():
+        config["labels_file"] = input_file_path / config["labels_file"]
+    if "sequence_file" in config.keys():
+        config["sequence_file"] = input_file_path / config["sequence_file"]
 
     original_config = deepcopy(config)
     out_config = execute(output_dir=input_file_path / "output", **original_config)
