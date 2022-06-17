3011d8233737a67978ec097cdfd6fd94cecc55c8
Sebastian Franz <sebastian11@online.de>
2022-06-17 09:45:34 +0200

    Removing unnecessary execute() function
    Moving the content of execute into parse_config_file_and_execute_run. Also removes the `    original_config = deepcopy(config)` statement because it seems to be redundant.

--- biotrainer/utilities/executer.py
+++ biotrainer/utilities/executer.py
@@ -2,8 +2,6 @@ import os
 import logging
 
 from pathlib import Path
-from copy import deepcopy
-from typing import Dict, Any
 
 from ..trainers import training_and_evaluation_routine
 from .config import validate_file, read_config_file, verify_config, write_config_file
@@ -18,19 +16,6 @@ __PROTOCOLS = {
 }
 
 
-def execute(output_dir: str = "output", protocol: str = "residue_to_class", **kwargs) -> Dict[str, Any]:
-    output_vars = deepcopy(locals())
-    output_vars.pop('kwargs')
-
-    output_dir = Path(output_dir)
-
-    if not output_dir.is_dir():
-        logger.info(f"Creating output dir: {output_dir}")
-        output_dir.mkdir(parents=True)
-
-    return training_and_evaluation_routine(**{**kwargs, **output_vars})
-
-
 def parse_config_file_and_execute_run(config_file_path: str):
     validate_file(config_file_path)
 
@@ -38,14 +23,23 @@ def parse_config_file_and_execute_run(config_file_path: str):
     config = read_config_file(config_file_path)
     verify_config(config, __PROTOCOLS)
 
+    # Make input file paths absolute
     input_file_path = Path(os.path.dirname(os.path.abspath(config_file_path)))
     if "labels_file" in config.keys():
         config["labels_file"] = str(input_file_path / config["labels_file"])
     if "sequence_file" in config.keys():
         config["sequence_file"] = str(input_file_path / config["sequence_file"])
 
-    original_config = deepcopy(config)
-    out_config = execute(output_dir=str(input_file_path / "output"), **original_config)
+    # Create output directory (if necessary)
+    output_dir = input_file_path / "output"
+    if not output_dir.is_dir():
+        logger.info(f"Creating output dir: {output_dir}")
+        output_dir.mkdir(parents=True)
+
+    # Run biotrainer pipeline
+    out_config = training_and_evaluation_routine(output_dir=str(output_dir), **config)
+
+    # Save output_variables in out.yml
     write_config_file(
         str(Path(out_config['output_dir']) / "out.yml"),
         out_config
