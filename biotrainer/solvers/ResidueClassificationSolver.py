d12d3ca541b497b06ed54cfaf5d1f48391333142
Sebastian Franz <sebastian11@online.de>
2022-06-23 09:01:23 +0200

    Reformatting if clause

--- biotrainer/solvers/ResidueClassificationSolver.py
+++ biotrainer/solvers/ResidueClassificationSolver.py
@@ -10,8 +10,9 @@ class ResidueClassificationSolver(Solver):
     def _transform_network_output(self, network_output: torch.Tensor) -> torch.Tensor:
 
         network_type = type(self.network).__name__
-        if network_type in ["FNN", "LogReg"] :
-            network_output = network_output.permute(0, 2, 1)  # (Batch_size x protein_Length x Number_classes) => (B x N x L)
+        if network_type in ["FNN", "LogReg"]:
+            # (Batch_size x protein_Length x Number_classes) => (B x N x L)
+            network_output = network_output.permute(0, 2, 1)
 
         return network_output
 
