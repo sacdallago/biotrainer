eef6c11c912a231f45be25e0a4e9819689aee5e5
Sebastian Franz <sebastian11@online.de>
2022-06-23 09:02:32 +0200

    Adding new constants script where PAD and MASK values are stored
    I thought it would make sense to use one constant for both MASK and LABELS PAD VALUE because they should always be the same.

--- biotrainer/utilities/__init__.py
+++ biotrainer/utilities/__init__.py
@@ -2,7 +2,7 @@ from .seeder import seed_all
 from .cuda_device import get_device
 from .FASTA import read_FASTA, get_attributes_from_seqrecords
 from .config import read_config_file
-
+from .constants import SEQUENCE_PAD_VALUE, MASK_AND_LABELS_PAD_VALUE
 
 __all__ = [
     'seed_all',
@@ -10,4 +10,6 @@ __all__ = [
     'read_FASTA',
     'read_config_file',
     'get_attributes_from_seqrecords',
+    'SEQUENCE_PAD_VALUE',
+    'MASK_AND_LABELS_PAD_VALUE',
 ]
