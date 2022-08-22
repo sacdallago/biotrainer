dc9cb55d834e81e9f5ab64f031d82969ea5c11ad
Sebastian Franz <sebastian11@online.de>
2022-08-22 14:59:55 +0200

    Adding pad_residues_embeddings collate function
    Does not pad labels in contrast to the pad_residue_embeddings function

--- biotrainer/datasets/collate_functions.py
+++ biotrainer/datasets/collate_functions.py
@@ -46,3 +46,36 @@ def pad_residue_embeddings(
 
     # Permute the embeddings to fit the LA architecture
     return seq_ids, padded_sequences, padded_labels, lengths
+
+
+def pad_residues_embeddings(
+        batch: List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]], batch_first=True
+) -> Tuple[List, torch.Tensor, torch.Tensor, torch.LongTensor]:
+    """
+
+    :param batch_first: first dimension of return value will be batch
+    :param batch: An iteration batch from the ResidueEmbeddingsDataset class [(ID, embedding, label)]
+    :return: Returns padded embeddings, labels, lengths and True/False mask for padding of input embeddings.
+    """
+
+    # Each element in "batch" is a tuple [(ID, embedding, target)]
+
+    # Sort the batch in the descending order
+    sorted_batch = sorted(batch, key=lambda x: x[1].shape[1], reverse=True)
+
+    # Sequence ids:
+    seq_ids = [x[0] for x in sorted_batch]
+
+    # Get each residue-embedding and pad it
+    sequences = [x[1] for x in sorted_batch]
+    padded_sequences = pad_sequence(sequences, batch_first=batch_first, padding_value=SEQUENCE_PAD_VALUE)
+
+    # Also need to store the length of each sequence
+    # This is later needed in order to unpad the sequences
+    lengths = torch.LongTensor([len(x) for x in sequences])
+
+    # Don't forget to grab the labels of the *sorted* batch
+    labels = [x[2] for x in batch]
+
+    # Permute the embeddings to fit the LA architecture
+    return seq_ids, padded_sequences, torch.stack(labels), lengths
