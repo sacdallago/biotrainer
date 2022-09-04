76d8b6ed86ba985ff20a716a8ff745aaa5eb2a5b
Joaquim Gomez <9joaquimgomez@gmail.com>
2022-09-04 18:39:51 +0200

    Adding extra metrics

--- biotrainer/solvers/ResidueClassificationSolver.py
+++ biotrainer/solvers/ResidueClassificationSolver.py
@@ -1,7 +1,7 @@
 import torch
 
 from typing import Dict, Union
-from torchmetrics import Accuracy, Precision, Recall, F1Score
+from torchmetrics import Accuracy, Precision, Recall, F1Score, SpearmanCorrCoef, MatthewsCorrCoef
 
 from .Solver import Solver
 from ..utilities import MASK_AND_LABELS_PAD_VALUE
@@ -12,11 +12,23 @@ class ResidueClassificationSolver(Solver):
     def __init__(self, *args, **kwargs):
         super(ResidueClassificationSolver, self).__init__(*args, **kwargs)
         # Init metrics
-        num_classes = kwargs['num_classes']
-        self.acc = Accuracy(average="micro", num_classes=num_classes)
-        self.precision = Precision(average="macro", num_classes=num_classes)
-        self.recall = Recall(average="macro", num_classes=num_classes)
-        self.f1_score = F1Score(average="macro", num_classes=num_classes)
+        self.num_classes = kwargs['num_classes']
+        self.acc = Accuracy(average="micro", num_classes=self.num_classes)
+
+        self.macro_precision = Precision(average="macro", num_classes=self.num_classes)
+        self.micro_precision = Precision(average="micro", num_classes=self.num_classes)
+        self.precision_per_class = Precision(average="none", num_classes=self.num_classes)
+
+        self.macro_recall = Recall(average="macro", num_classes=self.num_classes)
+        self.micro_recall = Recall(average="micro", num_classes=self.num_classes)
+        self.recall_per_class = Recall(average="none", num_classes=self.num_classes)
+        
+        self.macro_f1_score = F1Score(average="macro", num_classes=self.num_classes)
+        self.micro_f1_score = F1Score(average="macro", num_classes=self.num_classes)
+        self.f1_per_class = F1Score(average="none", num_classes=self.num_classes)
+
+        self.scc = SpearmanCorrCoef()
+        self.mcc = MatthewsCorrCoef(num_classes=self.num_classes)
 
     def _transform_network_output(self, network_output: torch.Tensor) -> torch.Tensor:
 
@@ -44,9 +56,30 @@ class ResidueClassificationSolver(Solver):
         masked_predicted = torch.masked_select(predicted, masks)
         masked_labels = torch.masked_select(labels, masks)
 
+        precision_per_class = self.precision_per_class(masked_predicted.cpu(), masked_labels.cpu())
+        precisions = {'- precission class {}'.format(i): precision_per_class[i] for i in range(self.num_classes)}
+
+        recall_per_class = self.recall_per_class(masked_predicted.cpu(), masked_labels.cpu())
+        recalls = {'- recall class {}'.format(i): recall_per_class[i] for i in range(self.num_classes)}
+
+        f1_per_class = self.f1_per_class(masked_predicted.cpu(), masked_labels.cpu())
+        f1scores = {'- f1_score class {}'.format(i): f1_per_class[i] for i in range(self.num_classes)}
+
         return {
             'accuracy': self.acc(masked_predicted.cpu(), masked_labels.cpu()).item(),
-            'precision': self.precision(masked_predicted.cpu(), masked_labels.cpu()).item(),
-            'recall': self.recall(masked_predicted.cpu(), masked_labels.cpu()).item(),
-            'f1_score': self.f1_score(masked_predicted.cpu(), masked_labels.cpu()).item()
+
+            'macro-precision': self.macro_precision(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            'micro-precision': self.macro_precision(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            **precisions,
+
+            'macro-recall': self.macro_recall(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            'micro-recall': self.micro_recall(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            **recalls,
+
+            'macro-f1_score': self.macro_f1_score(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            'micro-f1_score': self.micro_f1_score(masked_predicted.cpu(), masked_labels.cpu()).item(),
+            **f1scores,
+
+            'spearmans-corr-coeff': self.scc(masked_predicted.cpu().type(torch.FloatTensor), masked_labels.cpu().type(torch.FloatTensor)).item(),
+            'matthews-corr-coeff': self.mcc(masked_predicted.cpu(), masked_labels.cpu()).item(),
         }
