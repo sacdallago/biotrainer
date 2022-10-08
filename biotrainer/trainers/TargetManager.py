c29bef50170157e0453a0f74324c132b211a3be6
Sebastian Franz <sebastian.franz@tum.de>
2022-10-08 13:10:11 +0200

    Making protocol from TargetManager public
    Can now be used by trainer

--- biotrainer/trainers/TargetManager.py
+++ biotrainer/trainers/TargetManager.py
@@ -34,7 +34,7 @@ class TargetManager:
     def __init__(self, protocol: str, sequence_file: str,
                  labels_file: Optional[str] = None, mask_file: Optional[str] = None,
                  ignore_file_inconsistencies: Optional[bool] = False):
-        self._protocol = protocol
+        self.protocol = protocol
         self._sequence_file = sequence_file
         self._labels_file = labels_file
         self._mask_file = mask_file
@@ -46,7 +46,7 @@ class TargetManager:
         id2fasta = {protein.id: str(protein.seq) for protein in protein_sequences}
 
         # 1. Residue Level
-        if 'residue_' in self._protocol:
+        if 'residue_' in self.protocol:
             # Expect labels file to be in FASTA format, with each "residue" being the residue-associated-label
             label_sequences = read_FASTA(self._labels_file)
             self._id2attributes = get_attributes_from_seqrecords(label_sequences)
@@ -55,7 +55,7 @@ class TargetManager:
             self._id2target = {label.id: str(label.seq) for label in label_sequences}
 
             # a) Class output
-            if 'class' in self._protocol:
+            if 'class' in self.protocol:
                 class_labels_temp = set()
 
                 # Infer classes from data
@@ -87,7 +87,7 @@ class TargetManager:
                 raise NotImplementedError
 
         # 2. Sequence Level
-        elif 'sequence_' in self._protocol or 'residues_' in self._protocol:
+        elif 'sequence_' in self.protocol or 'residues_' in self.protocol:
 
             # In sequence task, split definitions are in sequence header, as well as target
             # For more info check file specifications!
@@ -95,7 +95,7 @@ class TargetManager:
 
             self._id2target = {seq_id: seq_vals["TARGET"] for seq_id, seq_vals in self._id2attributes.items()}
             # a) Class output
-            if 'class' in self._protocol:
+            if 'class' in self.protocol:
                 # Infer classes from data
                 self._class_labels = sorted(set(self._id2target.values()))
                 self.number_of_outputs = len(self._class_labels)
@@ -109,7 +109,7 @@ class TargetManager:
                                    for identifier, label in self._id2target.items()}  # classes idxs (zero-based)
 
             # b) Value output
-            elif 'value' in self._protocol:
+            elif 'value' in self.protocol:
                 self._id2target = {seq_id: float(seq_val) for seq_id, seq_val in self._id2target.items()}
                 self.number_of_outputs = 1
             else:
@@ -122,7 +122,7 @@ class TargetManager:
             raise Exception("Prediction targets not found or could not be extracted!")
 
     def _validate_targets(self, id2emb):
-        if 'residue_' in self._protocol:
+        if 'residue_' in self.protocol:
             """
             1. Check if number of embeddings and corresponding labels match:
                 # embeddings == # labels --> SUCCESS
@@ -199,22 +199,22 @@ class TargetManager:
         self.training_ids, self.validation_ids, self.testing_ids = get_split_lists(self._id2attributes)
 
         # Create datasets
-        train_dataset = get_dataset(self._protocol, {
+        train_dataset = get_dataset(self.protocol, {
             idx: (torch.tensor(id2emb[idx]), torch.tensor(self._id2target[idx])) for idx in self.training_ids
         })
-        val_dataset = get_dataset(self._protocol, {
+        val_dataset = get_dataset(self.protocol, {
             idx: (torch.tensor(id2emb[idx]), torch.tensor(self._id2target[idx])) for idx in self.validation_ids
         })
-        test_dataset = get_dataset(self._protocol, {
+        test_dataset = get_dataset(self.protocol, {
             idx: (torch.tensor(id2emb[idx]), torch.tensor(self._id2target[idx])) for idx in self.testing_ids
         })
 
         return train_dataset, val_dataset, test_dataset
 
     def compute_class_weights(self) -> torch.FloatTensor:
-        if 'class' in self._protocol:
+        if 'class' in self.protocol:
             # concatenate all labels irrespective of protein to count class sizes
-            if "residue_" in self._protocol:
+            if "residue_" in self.protocol:
                 counter = Counter(list(itertools.chain.from_iterable(
                     [list(labels) for labels in self._id2target.values()]
                 )))
