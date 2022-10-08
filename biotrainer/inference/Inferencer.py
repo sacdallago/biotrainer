c641c0113c9989ddf1b92ed8cd556396d76d1213
Sebastian Franz <sebastian.franz@tum.de>
2022-10-08 13:13:08 +0200

    Refactoring constructor and imports of Inferencer

--- biotrainer/inference/Inferencer.py
+++ biotrainer/inference/Inferencer.py
@@ -4,11 +4,12 @@ from typing import Union, Optional, Dict, Iterable, List
 
 from torch.utils.data import DataLoader
 
-from ..utilities import get_device
 from ..losses import get_loss
 from ..models import get_model
-from ..optimizers import get_optimizer
 from ..solvers import get_solver
+from ..utilities import get_device
+from ..optimizers import get_optimizer
+from ..trainers import revert_mappings
 from ..datasets import get_dataset, get_collate_function
 
 
@@ -24,19 +25,24 @@ class Inferencer:
           model_choice: str,
           embedder_name: str,
 
-          # These are from post-training
+          # These are from the output (out.yml) file
           log_dir: str,
-          class_int_to_string: Optional[Dict[int, str]],
+          class_int_to_string: Optional[Dict[int, str]] = None,
 
           # Fillers
           learning_rate: float = 1e-3,
           loss_choice: str = "cross_entropy_loss",
           optimizer_choice: str = "adam", batch_size: int = 128,
-          device: Union[None, str, torch.device] = None, embeddings_file_path: str = None,
+          device: Union[None, str, torch.device] = None,
 
           # Everything else
           **kwargs
     ):
+        self.protocol = protocol
+        self.device = get_device(device)
+        self.batch_size = batch_size
+        self.class_int2str = class_int_to_string
+        self.embedder_name = embedder_name
 
         model = get_model(
             protocol=protocol, model_choice=model_choice,
@@ -49,12 +55,6 @@ class Inferencer:
             protocol=protocol, optimizer_choice=optimizer_choice,
             learning_rate=learning_rate, model_parameters=model.parameters()
         )
-
-        self.device = get_device(device)
-        self.batch_size = batch_size
-        self.class_int_to_string = class_int_to_string
-        self.protocol = protocol
-        self.embedder_name = embedder_name
 
         self.solver = get_solver(
             protocol, network=model, optimizer=optimizer, loss_function=loss_function, device=self.device,
