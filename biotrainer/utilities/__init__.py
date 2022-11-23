ab0783d1fe42d8659c7ed940c162fea53b826a25
Sebastian <sebastian.franz@tum.de>
2022-11-23 09:42:13 +0100

    Exporting ConfigurationException properly

--- biotrainer/utilities/__init__.py
+++ biotrainer/utilities/__init__.py
@@ -1,14 +1,15 @@
 from .seeder import seed_all
 from .cuda_device import get_device
-from .FASTA import read_FASTA, get_attributes_from_seqrecords, get_attributes_from_seqrecords_for_protein_interactions
-from .config import read_config_file
+from .config import read_config_file, ConfigurationException
 from .constants import SEQUENCE_PAD_VALUE, MASK_AND_LABELS_PAD_VALUE
+from .FASTA import read_FASTA, get_attributes_from_seqrecords, get_attributes_from_seqrecords_for_protein_interactions
 
 __all__ = [
     'seed_all',
     'get_device',
     'read_FASTA',
     'read_config_file',
+    'ConfigurationException',
     'get_attributes_from_seqrecords',
     'get_attributes_from_seqrecords_for_protein_interactions',
     'SEQUENCE_PAD_VALUE',
