3745c5ce0903d83c2d62853294c761a7560b2e7f
Sebastian <sebastian.franz@tum.de>
2022-12-14 21:08:49 +0100

    Adding model factory class
    Model factory handles creation of model, loss_function and optimizer.
    Separating variables from training pipeline.
    Will most likely need a variable for learning_rate in the future for CV

--- biotrainer/trainers/__init__.py
+++ biotrainer/trainers/__init__.py
@@ -1,9 +1,11 @@
+from .model_factory import ModelFactory
 from .embeddings import download_embeddings
 from .target_manager_utils import revert_mappings
 from .trainer import training_and_evaluation_routine
 
 
 __all__ = [
+    'ModelFactory',
     'download_embeddings',
     'revert_mappings',
     'training_and_evaluation_routine',
