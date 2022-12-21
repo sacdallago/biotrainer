fbff6415aa77c861ef91f676744d8a9c9ea611e5
Sebastian Franz <sebastian.franz@tum.de>
2022-12-21 14:47:18 +0100

    Adding hyper parameter to SplitResult
    Necessary for nested cross validation

--- biotrainer/utilities/data_classes.py
+++ biotrainer/utilities/data_classes.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
 
 Split = namedtuple("Split", "name train val")
-SplitResult = namedtuple("SplitResult", "name, best_epoch_metrics, solver")
+SplitResult = namedtuple("SplitResult", "name, hyper_params, best_epoch_metrics, solver")
 DatasetSample = namedtuple("DatasetSample", "seq_id embedding target")
