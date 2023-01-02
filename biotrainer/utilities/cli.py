2618d596d28d0a9151a4863167c22feef55c5643
Sebastian <sebastian.franz@tum.de>
2023-01-02 15:43:03 +0100

    Adding optional cli action "--list_embedders"
    Shows all available embedders from bio_embeddings

--- biotrainer/utilities/cli.py
+++ biotrainer/utilities/cli.py
@@ -8,6 +8,14 @@ def headless_main(config_file_path: str):
     parse_config_file_and_execute_run(config_file_path=config_file_path)
 
 
+def _list_available_embedders():
+    from bio_embeddings.embed import __all__
+
+    for imported_embedder in __all__:
+        if "Interface" not in imported_embedder:
+            print(imported_embedder)
+
+
 def main(args=None):
     """
     Pipeline commandline entry point
@@ -17,11 +25,22 @@ def main(args=None):
     logging.captureWarnings(True)
 
     parser = argparse.ArgumentParser(description='Trains models on protein embeddings.')
-    parser.add_argument('config_path', metavar='/path/to/pipeline_definition.yml', type=str, nargs=1,
-                        help='The path to the config. For examples, see folder "parameter examples".')
+    parser.add_argument('--list_embedders', required=False, action='store_true',
+                        help='List all available embedders from bio_embeddings')
+    parser.add_argument('config_path', metavar='/path/to/pipeline_definition.yml', type=str, nargs='?',
+                        help='The path to the config. For examples, see folder "examples".')
+
     arguments = parser.parse_args()
 
-    parse_config_file_and_execute_run(arguments.config_path[0])
+    if arguments.list_embedders:
+        _list_available_embedders()
+        return
+
+    config_path = arguments.config_path
+    if not config_path:
+        parser.print_help()
+    else:
+        parse_config_file_and_execute_run(config_path)
 
 
 if __name__ == '__main__':
