9fe751c5bc2e3a943f1769b8543a20089e4887fa
Sebastian <sebastian.franz@tum.de>
2023-04-01 14:03:39 +0200

    Enabling download for all input files
    Sequence file, labels file, mask file and pretrained model can now be downloaded from the config file. Moving download_embeddings to executer and renaming it to _download_file

--- biotrainer/trainers/__init__.py
+++ biotrainer/trainers/__init__.py
@@ -1,11 +1,9 @@
 from .trainer import Trainer
 from .hp_manager import HyperParameterManager
-from .embeddings import download_embeddings
 from .target_manager_utils import revert_mappings
 
 __all__ = [
     'Trainer',
     'revert_mappings',
-    'download_embeddings',
     'HyperParameterManager',
 ]
