ea38af667ef56a6917a46c6c01457580d521c437
Sebastian <sebastian.franz@iteratec.com>
2023-12-20 17:28:52 +0100

    Removing custom embedder from trainer module

--- biotrainer/trainers/__init__.py
+++ biotrainer/trainers/__init__.py
@@ -1,5 +1,4 @@
 from .trainer import Trainer
-from .custom_embedder import CustomEmbedder
 from .hp_manager import HyperParameterManager
 from .target_manager_utils import revert_mappings
 
@@ -7,5 +6,4 @@ __all__ = [
     'Trainer',
     'revert_mappings',
     'HyperParameterManager',
-    'CustomEmbedder'
 ]
