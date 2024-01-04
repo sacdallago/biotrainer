af0e7d5fe5344029116d3e13ab5f393e0cd6c3f7
Sebastian <sebastian.franz@iteratec.com>
2024-01-04 16:35:44 +0100

    Removing deprecated list_available_embedders functionality

--- biotrainer/utilities/cli.py
+++ biotrainer/utilities/cli.py
@@ -7,29 +7,15 @@ def headless_main(config_file_path: str):
     parse_config_file_and_execute_run(config_file_path=config_file_path)
 
 
-def _list_available_embedders():
-    from bio_embeddings.embed import __all__
-
-    for imported_embedder in __all__:
-        if "Interface" not in imported_embedder:
-            print(imported_embedder)
-
-
 def main(args=None):
     """
     Pipeline commandline entry point
     """
     parser = argparse.ArgumentParser(description='Trains models on protein embeddings.')
-    parser.add_argument('--list_embedders', required=False, action='store_true',
-                        help='List all available embedders from bio_embeddings')
     parser.add_argument('config_path', metavar='/path/to/pipeline_definition.yml', type=str, nargs='?',
                         help='The path to the config. For examples, see folder "examples".')
 
     arguments = parser.parse_args()
-
-    if arguments.list_embedders:
-        _list_available_embedders()
-        return
 
     config_path = arguments.config_path
     if not config_path:
