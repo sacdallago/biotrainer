5a05fc85e71f0069b22daab4069963322fcf4b78
Sebastian <sebastian.franz@iteratec.com>
2024-01-04 15:46:16 +0100

    Adding is_device_cpu function

--- biotrainer/utilities/__init__.py
+++ biotrainer/utilities/__init__.py
@@ -1,6 +1,6 @@
 from .seeder import seed_all
 from .version import __version__
-from .cuda_device import get_device
+from .cuda_device import get_device, is_device_cpu
 from .data_classes import Split, SplitResult, DatasetSample
 from .constants import SEQUENCE_PAD_VALUE, MASK_AND_LABELS_PAD_VALUE, INTERACTION_INDICATOR, \
     METRICS_WITHOUT_REVERSED_SORTING
@@ -10,6 +10,7 @@ from .FASTA import read_FASTA, get_attributes_from_seqrecords, \
 __all__ = [
     'seed_all',
     'get_device',
+    'is_device_cpu',
     'read_FASTA',
     'get_attributes_from_seqrecords',
     'get_attributes_from_seqrecords_for_protein_interactions',
