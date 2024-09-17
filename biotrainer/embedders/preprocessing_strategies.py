e84aef3eea7da4139042bccbb982dfac31c5b2a6
Sebastian <sebastian.franz@tum.de>
2024-09-17 15:42:37 +0200

    Adding Rostlab/ProstT5 embedder support
    New preprocessing strategy and custom index removing

--- biotrainer/embedders/preprocessing_strategies.py
+++ biotrainer/embedders/preprocessing_strategies.py
@@ -15,3 +15,14 @@ def preprocess_sequences_without_whitespaces(sequences: Iterable[str]) -> List[s
     # Remove rare amino acids
     sequences_cleaned = [re.sub(r"[UZOB]", "X", sequence) for sequence in sequences]
     return sequences_cleaned
+
+
+def preprocess_sequences_for_prostt5(sequences: Iterable[str]) -> List[str]:
+    preprocessed_sequences = preprocess_sequences_with_whitespaces(sequences)
+
+    # We have AAs, so we need to tell the model we want to translate AA->3Di.
+    # To do so, we prepend "<AA2fold>" while keeping residues in upper-case.
+    prefixed_sequences = [
+        "<AA2fold>" + " " + seq.upper() for seq in preprocessed_sequences
+    ]
+    return prefixed_sequences
