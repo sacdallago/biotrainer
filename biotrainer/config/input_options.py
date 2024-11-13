2a551e0ddbf3ec16f57288824143c03a63ba3ab5
heispv <peymanvahidi1998@gmail.com>
2024-11-13 16:01:28 +0100

    Adding docstrings and type hints to InputOption subclasses.

--- biotrainer/config/input_options.py
+++ biotrainer/config/input_options.py
@@ -1,11 +1,18 @@
 from abc import ABC
-from typing import List, Any, Union
+from typing import List, Any, Union, Type
 
 from .config_option import FileOption, classproperty
 from ..protocols import Protocol
 
 
 class InputOption(FileOption, ABC):
+    """
+    Abstract base class for input-related configuration options.
+
+    Extends `FileOption` to provide a specialized framework for input options
+    within a protocol. This class serves as a foundation for specific input options
+    by setting common attributes and behaviors.
+    """
 
     @classproperty
     def category(self) -> str:
@@ -17,6 +24,13 @@ class InputOption(FileOption, ABC):
 
 
 class SequenceFile(InputOption, FileOption):
+    """
+    Configuration option for specifying the sequence file.
+
+    This option allows users to provide a sequence file, typically in FASTA format,
+    which contains the biological sequences to be used for training. It supports downloading
+    from URLs if permitted and ensures that the file adheres to the allowed formats.
+    """
 
     @classproperty
     def name(self) -> str:
@@ -40,6 +54,13 @@ class SequenceFile(InputOption, FileOption):
 
 
 class LabelsFile(InputOption, FileOption):
+    """
+    Configuration option for specifying the labels file.
+
+    This option allows users to provide a labels file, typically in FASTA format,
+    which contains the labels corresponding to the biological sequences. It supports
+    downloading from URLs if permitted and is conditionally required based on the selected protocol.
+    """
 
     @classproperty
     def name(self) -> str:
@@ -67,6 +88,14 @@ class LabelsFile(InputOption, FileOption):
 
 
 class MaskFile(InputOption, FileOption):
+    """
+    Configuration option for specifying the mask file.
+
+    This option allows users to provide a mask file, typically in FASTA format,
+    which contains masks corresponding to the biological sequences. It supports
+    downloading from URLs if permitted and is optional based on the selected protocol.
+    """
+
     @classproperty
     def name(self) -> str:
         return "mask_file"
@@ -92,4 +121,5 @@ class MaskFile(InputOption, FileOption):
         return False
 
 
-input_options: List = [SequenceFile, LabelsFile, MaskFile]
+# List of all input-related configuration options
+input_options: List[Type[InputOption]] = [SequenceFile, LabelsFile, MaskFile]
