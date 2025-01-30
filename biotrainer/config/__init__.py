578c025ba5878415336659cfcf90fbcb232b0e5f
Sebastian <sebastian.franz@tum.de>
2025-01-30 10:55:20 +0100

    Fixing config __init__

--- biotrainer/config/__init__.py
+++ biotrainer/config/__init__.py
@@ -2,6 +2,6 @@ from .configurator import Configurator
 from .config_exception import ConfigurationException
 
 __all__ = [
-    Configurator,
-    ConfigurationException
+    "Configurator",
+    "ConfigurationException"
 ]
