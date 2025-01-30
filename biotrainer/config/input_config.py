9c1b76e70077a28847b4e86cf2747c19fec42099
Sebastian <sebastian.franz@tum.de>
2025-01-30 15:03:46 +0100

    Moving type from config option to config constraints

--- biotrainer/config/input_config.py
+++ biotrainer/config/input_config.py
@@ -9,38 +9,38 @@ def input_config(protocol: Protocol) -> Tuple[ConfigKey, List[ConfigOption]]:
     return ConfigKey.ROOT, [
         ConfigOption(
             name="sequence_file",
-            type=str,
             description="Provide the sequence file in FASTA format, "
                         "which contains the biological sequences to be used for training.",
             category=input_category,
             required=False,
             is_file_option=True,
             constraints=ConfigConstraints(
+                type=str,
                 allowed_formats=[".fasta"]
             )
         ),
         ConfigOption(
             name="labels_file",
-            type=str,
             description="Provide a labels file in FASTA format, "
                         "which contains the labels corresponding to residues for each sequence.",
             category=input_category,
             required=False,
             is_file_option=True,
             constraints=ConfigConstraints(
+                type=str,
                 allowed_formats=[".fasta"],
                 allowed_protocols=Protocol.per_residue_protocols()
             )
         ),
         ConfigOption(
             name="mask_file",
-            type=str,
             description="Provide the mask file in FASTA format, "
                         "which contains the mask corresponding to residues for each sequence.",
             category=input_category,
             required=False,
             is_file_option=True,
             constraints=ConfigConstraints(
+                type=str,
                 allowed_formats=[".fasta"],
                 allowed_protocols=Protocol.per_residue_protocols()
             )
--- biotrainer/config/training_config.py
+++ biotrainer/config/training_config.py
@@ -9,73 +9,82 @@ def training_config(protocol: Protocol) -> Tuple[ConfigKey, List[ConfigOption]]:
     return ConfigKey.ROOT, [
         ConfigOption(
             name="num_epochs",
-            type=int,
             description="Define the number of epochs to train the model.",
             category=training_category,
             required=False,
             default=200,
             allow_hyperparameter_optimization=True,
-            constraints=ConfigConstraints(gt=0)
+            constraints=ConfigConstraints(
+                type=int,
+                gt=0)
         ),
         ConfigOption(
             name="batch_size",
-            type=int,
             description="Define the batch size used for model training.",
             category=training_category,
             required=False,
             default=128,
             allow_hyperparameter_optimization=True,
-            constraints=ConfigConstraints(gt=0)
+            constraints=ConfigConstraints(
+                type=int,
+                gt=0)
         ),
         ConfigOption(
             name="patience",
-            type=int,
             description="Define the number of epochs to wait before early stopping.",
             category=training_category,
             required=False,
             default=10,
             allow_hyperparameter_optimization=True,
-            constraints=ConfigConstraints(gt=0)
+            constraints=ConfigConstraints(
+                type=int,
+                gt=0)
         ),
         ConfigOption(
             name="shuffle",
-            type=bool,
             description="Define whether to shuffle the training data before each training epoch.",
             category=training_category,
             required=False,
-            default=True
+            default=True,
+            constraints=ConfigConstraints(
+                type=bool,
+            )
         ),
         ConfigOption(
             name="use_class_weights",
-            type=bool,
             description="Define whether to use class weights for training, "
                         "which can be useful for handling imbalanced datasets.",
             category=training_category,
             required=False,
             default=False,
             allow_hyperparameter_optimization=True,
+            constraints=ConfigConstraints(
+                type=bool,
+            )
         ),
         ConfigOption(
             name="auto_resume",
-            type=bool,
             description="Define whether to to automatically resume training from the "
                         "last checkpoint in case of interruptions.",
             category=training_category,
             required=False,
-            default=False
+            default=False,
+            constraints=ConfigConstraints(
+                type=bool,
+            )
         ),
         ConfigOption(
             name="pretrained_model",
-            type=str,
             description="Define if a pretrained model should be loaded.",
             category=training_category,
             required=False,
             default="",
-            constraints=ConfigConstraints(allowed_formats=["safetensors"])
+            constraints=ConfigConstraints(
+                type=str,
+                allowed_formats=["safetensors"])
         ),
         ConfigOption(
             name="limited_sample_size",
-            type=int,
             description="Define a maximum number of samples to be used for training from the training dataset. "
                         "Useful for example to test, if a model is able to overfit. "
                         "A value of -1 indicates no limit.",
@@ -83,6 +92,7 @@ def training_config(protocol: Protocol) -> Tuple[ConfigKey, List[ConfigOption]]:
             required=False,
             default=-1,
             constraints=ConfigConstraints(
+                type=int,
                 gte=-1  # Allows -1 or positive values
             )
         )
