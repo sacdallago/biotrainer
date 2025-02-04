a298a8d9550faa548a97dfa8d1cc2a44bd13a514
Sebastian <sebastian.franz@tum.de>
2025-02-04 16:27:43 +0100

    Creating bootstrapper and adapting to new metrics calculation

--- biotrainer/validations/__init__.py
+++ biotrainer/validations/__init__.py
@@ -1,6 +1,8 @@
+from .bootstrapper import Bootstrapper
 from .sanity_checker import SanityChecker, SanityException
 
 __all__ = [
+    "Bootstrapper",
     "SanityChecker",
     "SanityException"
 ]
