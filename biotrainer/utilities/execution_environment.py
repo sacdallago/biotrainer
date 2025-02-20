e2ec498a06845f2e96b3f274142134c3905d5994
Sebastian <sebastian.franz@tum.de>
2025-02-20 14:26:24 +0100

    Adding execution environment to check for notebook execution

---
+++ biotrainer/utilities/execution_environment.py
@@ -0,0 +1,11 @@
+def is_running_in_notebook() -> bool:
+    try:
+        # This will only exist in Jupyter environments
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':  # Jupyter notebook or qtconsole
+            return True
+        elif shell == 'TerminalInteractiveShell':  # IPython terminal
+            return False
+    except NameError:
+        return False
+    return False
