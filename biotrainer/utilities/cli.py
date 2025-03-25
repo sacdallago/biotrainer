5cbb0ce86c386978f0009cb28bcd2730497b6abc
Sebastian <sebastian.franz@tum.de>
2025-03-25 14:11:20 +0100

    Adding custom trainer function
    By being able to provide a custom trainer, almost all functionalities of the training can be changed and overwritten.

--- biotrainer/utilities/cli.py
+++ biotrainer/utilities/cli.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from typing import Union, Dict, Any
+from typing import Union, Dict, Any, Callable
 
 from .executer import parse_config_file_and_execute_run
 
@@ -12,6 +12,21 @@ def headless_main(config: Union[str, Path, Dict[str, Any]]) -> Dict[str, Any]:
     @param config: Biotrainer configuration file path or config dict
     """
     return parse_config_file_and_execute_run(config)
+
+
+def headless_main_with_custom_trainer(config: Union[str, Path, Dict[str, Any]],
+                                      custom_trainer_function: Callable) -> Dict[str, Any]:
+    """
+    Entry point for usage in scripts with a function to create a custom trainer.
+    The custom trainer function must take the trainer parameters as input and return a (subclass of) Trainer e.g.
+    custom_trainer_function = lambda hp_manager, output_vars, config: Trainer(hp_manager=hp_manager,
+                                                                              output_vars=output_vars,
+                                                                              **config
+                                                                              )
+
+    @param config: Biotrainer configuration file path or config dict
+    """
+    return parse_config_file_and_execute_run(config, custom_trainer_function=custom_trainer_function)
 
 
 def main(args=None):
