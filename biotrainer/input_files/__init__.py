bd1d2e7b7552b4d814dde0ad878af365946d90c5
Sebastian <sebastian.franz@tum.de>
2025-06-10 14:41:09 +0200

    Adding write_FASTA function for biotrainer sequence records

--- biotrainer/input_files/__init__.py
+++ biotrainer/input_files/__init__.py
@@ -1,6 +1,6 @@
 from .biotrainer_seq_record import BiotrainerSequenceRecord
 from .convert_deprecated_fastas import convert_deprecated_fastas
-from .fasta import read_FASTA
+from .fasta import read_FASTA, write_FASTA
 from .utils import get_split_lists, merge_protein_interactions
 from .hf_dataset_to_fasta import process_hf_dataset_to_fasta
 
@@ -8,6 +8,7 @@ __all__ = [
     "BiotrainerSequenceRecord",
     "convert_deprecated_fastas",
     "read_FASTA",
+    "write_FASTA",
     "get_split_lists",
     "merge_protein_interactions",
     "process_hf_dataset_to_fasta",
