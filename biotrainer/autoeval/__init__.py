cdfec240547a8af09243098dbd6a0dd8112aaa92
Sebastian <sebastian.franz@tum.de>
2025-06-27 09:09:51 +0200

    Changing autoeval to generator function to yield progress

--- biotrainer/autoeval/__init__.py
+++ biotrainer/autoeval/__init__.py
@@ -1,6 +1,8 @@
+from .autoeval_progress import AutoEvalProgress
 from .autoeval import autoeval_pipeline, available_frameworks, get_unique_framework_sequences
 
 __all__ = ["autoeval_pipeline",
            "available_frameworks",
            "get_unique_framework_sequences",
+           "AutoEvalProgress",
            ]
