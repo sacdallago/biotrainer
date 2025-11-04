e0b8b667369d449ae6e481da00ffee9c74d84977
Sebastian <sebastian.franz@tum.de>
2025-11-04 13:23:08 +0100

    feat: Adding input_data config option to provide sequence records directly in code

--- biotrainer/config/input_config.py
+++ biotrainer/config/input_config.py
@@ -19,4 +19,14 @@ def input_config(protocol: Protocol) -> Tuple[ConfigKey, List[ConfigOption]]:
                 allowed_formats=[".fasta"]
             )
         ),
+        ConfigOption(
+            name="input_data",
+            description="Provide the input data directly as BiotrainerSequenceRecords.",
+            category=input_category,
+            required=False,  # Can be replaced by hf_dataset config
+            is_file_option=False,
+            constraints=ConfigConstraints(
+                type=List,
+            )
+        ),
     ]
