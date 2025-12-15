e800016467b4b94dbea18ce98c8e2a1c3c570c2e
Sebastian <sebastian.franz@tum.de>
2025-12-15 15:54:46 +0100

    feat: Adding conservation and disorder pbc datasets

--- biotrainer/autoeval/pbc/pbc_datasets.py
+++ biotrainer/autoeval/pbc/pbc_datasets.py
@@ -1,6 +1,14 @@
 from ...protocols import Protocol
 
 PBC_DATASETS = {
+    "conservation": {
+        "evaluation_metric": "accuracy",
+        "protocol": Protocol.residue_to_class,
+    },
+    "disorder": {
+        "evaluation_metric": "spearmans-corr-coeff",
+        "protocol": Protocol.residue_to_value,
+    },
     "scl": {
         "evaluation_metric": "accuracy",
         "protocol": Protocol.sequence_to_class,
