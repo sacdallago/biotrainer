8254afb8e267de71fe35e309d110c7cd1b8bdc6c
Sebastian <sebastian.franz@tum.de>
2025-12-15 14:39:19 +0100

    test: Adding test for direct embedding input data

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,13 +1,22 @@
+import torch
 import unittest
 import tempfile
+import numpy as np
 
 from copy import deepcopy
 
-from biotrainer.input_files import BiotrainerSequenceRecord
+
 from biotrainer.utilities.cli import train
+from biotrainer.input_files import BiotrainerSequenceRecord
+from biotrainer.utilities import FeatureScaler
 
 s2c_config = {'protocol': 'sequence_to_class',
               'model_choice': 'FNN',
+              'embedder_name': 'one_hot_encoding',
+              'input_file': "test_input_files/scl_subset/scl_rand.fasta"}
+
+rs2c_config = {'protocol': 'residues_to_class',
+              'model_choice': 'LightAttention',
               'embedder_name': 'one_hot_encoding',
               'input_file': "test_input_files/scl_subset/scl_rand.fasta"}
 
@@ -61,6 +70,30 @@ class TrainingTests(unittest.TestCase):
             BiotrainerSequenceRecord(seq_id="Seq5", seq="PRTE", attributes={"TARGET": "Soluble", "SET": "val"}),
             BiotrainerSequenceRecord(seq_id="Seq6", seq="MMALSM", attributes={"TARGET": "Membrane", "SET": "test"}),
             BiotrainerSequenceRecord(seq_id="Seq7", seq="PRSEQ", attributes={"TARGET": "Soluble", "SET": "test"}),
+        ]
+        with tempfile.TemporaryDirectory() as tmp_dir_name:
+            config = deepcopy(s2c_config)
+            config.pop("input_file")
+            config["input_data"] = input_data
+            result = train(config=config)
+            self.assertEqual(result['config']['input_data'], len(input_data))
+
+    def test_direct_input_data_with_embeddings(self):
+        input_data = [
+            BiotrainerSequenceRecord(seq_id="Seq1", seq="MMALSLALM", attributes={"TARGET": "Membrane", "SET": "train"},
+                                     embedding=[1, 2, 3]),
+            BiotrainerSequenceRecord(seq_id="Seq2", seq="PRTEIN", attributes={"TARGET": "Membrane", "SET": "train"},
+                                     embedding=[4, 5, 6]),
+            BiotrainerSequenceRecord(seq_id="Seq3", seq="PRT", attributes={"TARGET": "Soluble", "SET": "train"},
+                                     embedding=[7, 8, 9]),
+            BiotrainerSequenceRecord(seq_id="Seq4", seq="SEQWENCE", attributes={"TARGET": "Membrane", "SET": "val"},
+                                     embedding=[10, 11, 12]),
+            BiotrainerSequenceRecord(seq_id="Seq5", seq="PRTE", attributes={"TARGET": "Soluble", "SET": "val"},
+                                     embedding=[13, 14, 15]),
+            BiotrainerSequenceRecord(seq_id="Seq6", seq="MMALSM", attributes={"TARGET": "Membrane", "SET": "test"},
+                                     embedding=torch.tensor([16, 17, 18])),
+            BiotrainerSequenceRecord(seq_id="Seq7", seq="PRSEQ", attributes={"TARGET": "Soluble", "SET": "test"},
+                                     embedding=np.array([19, 20, 21])),
         ]
         with tempfile.TemporaryDirectory() as tmp_dir_name:
             config = deepcopy(s2c_config)
