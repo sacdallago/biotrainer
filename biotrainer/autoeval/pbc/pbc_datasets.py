8f6f26f93d9c23c1c4968f4a75c4a4df2fb8ec43
Sebastian <sebastian.franz@tum.de>
2025-12-16 20:36:13 +0100

    feat: Adding membrane task to PBC

--- biotrainer/autoeval/pbc/pbc_datasets.py
+++ biotrainer/autoeval/pbc/pbc_datasets.py
@@ -9,6 +9,10 @@ PBC_DATASETS = {
         "evaluation_metric": "spearmans-corr-coeff",
         "protocol": Protocol.residue_to_value,
     },
+    "membrane": {
+        "evaluation_metric": "macro-f1_score",
+        "protocol": Protocol.residue_to_class,
+    },
     "scl": {
         "evaluation_metric": "accuracy",
         "protocol": Protocol.sequence_to_class,
