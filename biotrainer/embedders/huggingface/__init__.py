69d07152a6e3da3068e602ea0cc027e74eac59e1
Sebastian <sebastian.franz@tum.de>
2025-12-30 18:28:46 +0100

    feat: Adding optimized embedder interface
    Interface to detect itself from the provided embedder_name and some utility method(s)

--- biotrainer/embedders/huggingface/__init__.py
+++ biotrainer/embedders/huggingface/__init__.py
@@ -1,3 +1,4 @@
 from .huggingface_transformer_embedder import HuggingfaceTransformerEmbedder
+from .ProtT5 import ProtT5
 
-__all__ = ["HuggingfaceTransformerEmbedder"]
+__all__ = ["HuggingfaceTransformerEmbedder", "ProtT5"]
