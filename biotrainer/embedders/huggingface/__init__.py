febf69f02c38cf29ce3007cbf2f016d0fed9c92b
Sebastian <sebastian.franz@tum.de>
2026-01-13 10:16:35 +0100

    feat: Adding optimized versions for ProstT5 and ESM-2 model family

--- biotrainer/embedders/huggingface/__init__.py
+++ biotrainer/embedders/huggingface/__init__.py
@@ -1,4 +1,6 @@
-from .huggingface_transformer_embedder import HuggingfaceTransformerEmbedder
+from .ESM2 import ESM2
 from .ProtT5 import ProtT5
+from .ProstT5 import ProstT5
+from .huggingface_transformer_embedder import HuggingfaceTransformerEmbedder
 
-__all__ = ["HuggingfaceTransformerEmbedder", "ProtT5"]
+__all__ = ["HuggingfaceTransformerEmbedder", "ProtT5", "ProstT5", "ESM2"]
